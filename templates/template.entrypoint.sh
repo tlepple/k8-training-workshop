c2dd5f3f6b0d8c88c7d506e4602ae88a88807310
tlepple <49572881+tlepple@users.noreply.github.com>
2024-12-15 13:32:39 -0600

    Update template.entrypoint.sh

--- templates/template.entrypoint.sh
+++ templates/template.entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Define repository URL using PAT from environment variable
-REPO_URL="dev.azure.com/Nexus-One/NX1%20Special%20Projects/_git/Test"
+#  clear some older creds
+git credential-cache exit
+
+REPO_URL="dev.azure.com/Nexus-One/NX1%20Special%20Projects/_git/aws_ad_auth"
 AZ_USERNAME="<YOUR-USERNAME-HERE>"
 AZURE_PAT="<YOUR-PAT-HERE>"
 
@@ -13,7 +16,7 @@ if [ -z "$(ls -A /app/pim)" ]; then
 else
   echo "Directory /app/pim already contains files. Updating the repository..."
 #  git -C /app/pim pull
-   git -C /app/pim pull https://$AZ_USERNAME:$AZURE_PAT@dev.azure.com/Nexus-One/NX1%20Special%20Projects/_git/aws_ad_auth
+   git -C /app/pim pull --rebase https://$AZ_USERNAME:$AZURE_PAT@dev.azure.com/Nexus-One/NX1%20Special%20Projects/_git/aws_ad_auth
 fi
 
 # Execute the main command
